f358a0adf1197be199130b8d6a711d58f1745410
Frans Caisar Ramadhan <frans.ramadhan@traveloka.com>
2019-09-04 18:11:29 +0700

    add terratest

--- examples/main.tf
+++ examples/main.tf
@@ -3,11 +3,14 @@ module "this" {
   product_domain = "pay"
   environment    = "stg"
   service_name   = "paytest"
-  role_names     = ["Developer","Manager"]
+  role_names     = ["MidasDev"]
   kms_key_id     = "alias/aws/ssm"
 
   parameter_names = {
     name1 = "description1"
     name2 = "description2"
   }
+}
+output "name" {
+  value = "${module.this.parameter_names}"
 }
